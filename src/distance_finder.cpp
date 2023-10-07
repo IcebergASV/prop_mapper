1991b9d4d0108d49331edb1b4e386cd675ac07e1
Grace Pearcey <pearcey.grace@gmail.com>
2023-10-07 12:50:08 -0400

    moved prop validity check to separate function

--- src/distance_finder.cpp
+++ src/distance_finder.cpp
@@ -36,17 +36,20 @@ public:
         // get ROS parameters
         private_nh_.param<double>("angle_error_adjustment", angle_error_adjustment_, 0.0);
 
-        private_nh_.param<std::string>("prop_topic", prop_topic_, "/prop_angle_range");
+        // Specify ROS topic names - using parameters for this so that we can change names from launch files
+        private_nh_.param<std::string>("prop_topic", prop_angles_topic_, "/prop_angle_range");
         private_nh_.param<std::string>("scan_topic", scan_topic_, "/scan");
         
-
+        // set up subscribers
         sub_scan_ = nh_.subscribe(scan_topic_, 1, &CoordFinder::scanCallback, this);
-        sub_prop_ = nh_.subscribe(prop_topic_, 1, &CoordFinder::propCallback, this);
-        pub_prop_closest_ = nh_.advertise<geometry_msgs::Vector3>("/prop_xy_dist", 1);
+        sub_prop_angles_ = nh_.subscribe(prop_topic_, 1, &CoordFinder::propCallback, this);
+
+        // set up publishers
+        pub_prop_xy_dist_ = nh_.advertise<geometry_msgs::Vector3>("/prop_xy_dist", 1);
     }
 
     void spin() {
-        ros::Rate rate(10); // 10 Hz
+        ros::Rate rate(10); // Spin at 10 Hz
         while (ros::ok()) {
             ros::spinOnce();
             rate.sleep();
@@ -55,65 +58,90 @@ public:
     
 
 private:
+
     ros::NodeHandle nh_;                            //!< node handle
     ros::NodeHandle private_nh_;                    //!< private node handle
     ros::Subscriber sub_scan_;                      //!< subscriber to scan topic
-    ros::Subscriber sub_prop_;
-    ros::Publisher pub_prop_closest_;
-    std::string prop_topic_;
-    std::string scan_topic_;
-    double laser_angle_min_;
-    double laser_angle_max_;
-    double laser_angle_increment_;
-    double angle_error_adjustment_;
-    prop_follower::PropAngleRange prop_msg_;
-    sensor_msgs::LaserScan scan_msg_;
+    ros::Subscriber sub_prop_angles_;               //!< subscriber to prop's angle ranges from bounding boxes
+    ros::Publisher pub_prop_xy_dist_;               //!< publisher for prop's relative x and y distances
+    std::string prop_angles_topic_;                 //!< prop angles topic name
+    std::string scan_topic_;                        //!< scan topic name
+    
+    double laser_angle_min_;                        //!< minimum angle of the LiDAR
+    double laser_angle_max_;                        //!< maximum angle of the LiDAR
+    double laser_angle_increment_;                  //!< increment between readings within a LiDAR scan
+    double angle_error_adjustment_;                 //!< used to expand the angles provided from bounding boxes
+    
+    prop_follower::PropAngleRange prop_angles_msg_; //!< prop angles message from bounding boxes
+    sensor_msgs::LaserScan scan_msg_;               //!< 
 
-    std::string TAG = "COORD_FINDER: ";
+    std::string TAG = "DISTANCE_FINDER: ";          //!< tag for logging and debug messages
 
+    //-----------------------------------------------------------------------------------------------------
+    // Helper Functions
+    //-----------------------------------------------------------------------------------------------------
+   
     /**
-    * @brief Updates the prop label and angles
+    @brief Checks if PropAngleRange message is valid
+    @returns true if message is valid    
+    */
+    bool isValidProp(const prop_follower::PropAngleRange::ConstPtr& msg)
+    {
+        bool valid_msg = true;
+
+        if (msg.prop_label.empty()) {
+            ROS_WARN_STREAM(TAG << "Invalid PropInProgress message received - Prop type is empty");
+            valid_msg = false;
+        }
+        if (std::isnan(msg.theta_small)) {
+            ROS_WARN_STREAM(TAG << "Invalid PropInProgress message received - theta 1 is empty");
+            valid_msg = false;
+        }
+        if (std::isnan(msg.theta_large)) {
+            ROS_WARN_STREAM(TAG << "Invalid PropInProgress message received - theta 2 is empty");
+            valid_msg = false;
+        }
+        return valid_msg;
+    }
+
+    // ----------------------------------------------------------------------------------------------------
+    // Callback functions
+    // ----------------------------------------------------------------------------------------------------
+    /**
+    * @brief Gets the prop label and angles
     * 
     * Runs whenever a message is published on /prop_angle_range 
     */
     void propCallback(const prop_follower::PropAngleRange::ConstPtr& msg) {
-        // save the PropInProgress message for later use
-        prop_msg_ = *msg;
-        ROS_DEBUG_STREAM(TAG << "Received PropInProgress message with theta_small=" << prop_msg_.theta_small << " and theta_large=" << prop_msg_.theta_large);
+        // save the PropAngleRange message for later use
+        prop_angles_msg_ = *msg;
+        ROS_DEBUG_STREAM(TAG << "Received PropInProgress message with theta_small=" << prop_angles_msg_.theta_small << " and theta_large=" << prop_angles_msg_.theta_large);
     }
 
     /**
-    * @brief Finds the nearest detected point and converts it to x and y coordinates. 
+    * @brief Finds the nearest detected point within the angle range and converts it to x and y coordinates. 
     * 
     * Runs whenever message is published on /scan topic. 
-    * Publishes a Vector3 message containing NED coordinates. (North East Down)
+    * Publishes a Vector3 message containing NED coordinates of the detected prop. (North East Down)
     * 
     * */
     void scanCallback(const sensor_msgs::LaserScan::ConstPtr& msg) {
+        
+        // save the scan message
+        scan_msg_ = *msg;
+
+        // get LiDAR specs
         laser_angle_min_ = scan_msg_.angle_min;
         laser_angle_max_ = scan_msg_.angle_max;
-
-        // save the scan message for later use
-        scan_msg_ = *msg;
         laser_angle_increment_ = scan_msg_.angle_increment;
 
-        // check if the PropInProgress message is valid
-        if (prop_msg_.prop_label.empty()) {
-            ROS_WARN_STREAM(TAG << "Invalid PropInProgress message received - Prop type is empty");
-            return;
-        }
-        if (std::isnan(prop_msg_.theta_small)) {
-            ROS_WARN_STREAM(TAG << "Invalid PropInProgress message received - theta 1 is empty");
-            return;
-        }
-        if (std::isnan(prop_msg_.theta_large)) {
-            ROS_WARN_STREAM(TAG << "Invalid PropInProgress message received - theta 2 is empty");
-            return;
-        }
+        // display warning and exit function if PropAngleRange message is invalid
+        if(!isValidProp(prop_angles_msg_)) {return;}
 
         //add a safety range onto the bounding box angles
-        double index1_angle = prop_msg_.theta_small + angle_error_adjustment_;
-        double index2_angle = prop_msg_.theta_large - angle_error_adjustment_;
+        double index1_angle = prop_angles_msg_.theta_small + angle_error_adjustment_;
+        double index2_angle = prop_angles_msg_.theta_large - angle_error_adjustment_;
+
         // calculate the range indexes for the given theta angles
         double steps = (laser_angle_max_ * 2) / laser_angle_increment_; 
         int index1 = (int)(((index1_angle + (laser_angle_max_ - (M_PI/2))) / (laser_angle_max_*2))* steps);
@@ -172,7 +200,7 @@ private:
         prop_coords_msg.x = closest_distance*sin(closest_angle); //North
         prop_coords_msg.y = closest_distance*cos(closest_angle); //East 
         prop_coords_msg.z = 0; //Down
-        pub_prop_closest_.publish(prop_coords_msg);
+        pub_prop_xy_dist_.publish(prop_coords_msg);
     }
 
 
